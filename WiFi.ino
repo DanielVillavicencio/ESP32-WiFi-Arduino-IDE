a639ee0784393c0a261960fea0410fd9b7c5a512
DanielVillavicencio <134111302+DanielVillavicencio@users.noreply.github.com>
2024-06-04 00:43:29 -0600

    WiFi.ino

--- WiFi.ino
+++ WiFi.ino
@@ -1,18 +1,15 @@
 #include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-Serial.println("Iniciando conexion a WiFi");
-WiFi.begin("Lab","B0980z2(");
-while(WiFi.status() != WL_CONNECTED){
-  delay(500);
-  Serial.print(".");
-}
-Serial.println("CONECTADO");
+  Serial.begin(115200);
+  Serial.println("Iniciando conexion a WiFi");
+  WiFi.begin("SSID","PASSWORD");
+    while(WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
+    }
+  Serial.println("CONECTADO");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
